244d74876a5e2b67a2a59f35a9cc02b237fffe68
Ted Hall <ted@shopbottools.com>
2026-01-20 09:06:35 -0800

    Normalize line endings to LF

--- js/message_g2.js
+++
@@ -1,5 +0,0 @@
-//..................................... SEND Message to G2 Stream VIA Manual RUNTIME-Raw
-function sendG2message (message) {
-    console.log("sending ====> ", message);
-    fabmo.manualRunGCode(message)
-}
